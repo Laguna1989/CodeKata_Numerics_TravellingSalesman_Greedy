792841ac8d71be72d86dab42ed5a22d9514b347f
Paul Kerchen <paul@whirlingchair.com>
2021-08-21 10:25:16 -0400

    Introduced random cities

--- python/src/city.py
+++ python/src/city.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from random import random
 
 
 class City:
@@ -6,10 +7,19 @@ class City:
         self.x = x
         self.y = y
 
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
 
 def compute_distance(cityA: City, cityB: City):
     return sqrt((cityA.x - cityB.x) ** 2 + (cityA.y - cityB.y) ** 2)
 
 
-def create_cities(amount: int):
-    return [City(0.0, 0.0)] * amount
+def create_cities(amount: int, rng=random):
+    cities = []
+    for i in range(amount):
+        cities.append(City(rng(), rng()))
+    return cities
--- python/tests/create_cities_test.py
+++ python/tests/create_cities_test.py
@@ -7,3 +7,19 @@ def test_create_cities_returns_correct_amount_of_cities(amount):
     cities = create_cities(amount)
 
     assert len(cities) == amount
+
+
+@pytest.mark.parametrize("amount", [2, 5, 10, 20, 30])
+def test_create_cities_returns_unique_cities(amount):
+    cities = create_cities(amount)
+
+    assert len(set(cities)) == len(cities)
+
+
+
+@pytest.mark.parametrize("amount", [2, 5, 10, 20, 30])
+def test_create_cities_returns_random_cities(amount):
+    citiesA = create_cities(amount)
+    citiesB = create_cities(amount)
+
+    assert not set(citiesA).intersection(set(citiesB))
