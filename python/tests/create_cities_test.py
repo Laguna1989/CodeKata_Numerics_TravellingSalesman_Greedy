d397b13a4b6d5825d204e2e15abedf4c9dbb87a8
Simon Weis <laguna_1989@gmx.net>
2021-08-21 16:37:11 +0200

    Add function to calculate full travel length

--- python/tests/create_cities_test.py
+++ python/tests/create_cities_test.py
@@ -2,6 +2,9 @@ import pytest
 
 from city import create_cities
 
+import random
+
+
 @pytest.mark.parametrize("amount", [1, 2, 5, 10, 20, 30])
 def test_create_cities_returns_correct_amount_of_cities(amount):
     cities = create_cities(amount)
@@ -16,9 +19,9 @@ def test_create_cities_returns_unique_cities(amount):
     assert len(set(cities)) == len(cities)
 
 
-
-@pytest.mark.parametrize("amount", [2, 5, 10, 20, 30])
-def test_create_cities_returns_random_cities(amount):
+@pytest.mark.parametrize("seed, amount", [(1, 1), (1, 2), (2, 2), (1, 5), (2, 10)])
+def test_create_cities_returns_random_cities(seed, amount):
+    random.seed(seed)
     citiesA = create_cities(amount)
     citiesB = create_cities(amount)
 
